24ee394dbd89956b68b4f19ba88884168021195c
Jayson Reeves <jericreeves@gmail.com>
2022-02-05 22:06:24 -0500

    working code quz homework

---
+++ assets/js/questions.js
@@ -0,0 +1,87 @@
+// questions from: https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
+
+var questions = [
+  {
+    title: "Which of the following is an advantage of using JavaScript?",
+    choices: [
+      "less server interaction",
+      "immediate feedback to the user",
+      "increased interactivity",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    title:
+      "Which of the following is a valid type of function JavaScript supports?",
+    choices: [
+      "named function",
+      "anonymous function",
+      "both 1. & 2.",
+      "neither 1. nor 2.",
+    ],
+    answer: "both 1. & 2.",
+  },
+  {
+    title:
+      "Which of the following types of variables is visible only within a function where it is defined?",
+    choices: [
+      "global variable",
+      "local variable",
+      "both 1. & 2.",
+      "neither 1. nor 2.",
+    ],
+    answer: "local variable",
+  },
+  {
+    title: "Which of the following code snippets creates an object?",
+    choices: [
+      "var book = Object();",
+      "var book = new Object();",
+      "var book = new OBJECT();",
+      "var book = new Book();",
+    ],
+    answer: "var book = new Object();",
+  },
+  {
+    title:
+      "Which of the following functions of String objects returns a number indicating the Unicode value of the character at the given index?",
+    choices: ["charAt()", "charCodeAt()", "concat()", "indexOf()"],
+    answer: "charCodeAt()",
+  },
+  {
+    title:
+      "Which of the following functions of String objects returns a number indicating whether a reference string comes before or after or is the same as the given string in sort order?",
+    choices: ["localeCompare()", "search()", "substr()", "concat()"],
+    answer: "localeCompare()",
+  },
+  {
+    title:
+      "Which of the following functions of String objects returns the primitive value of the specified object?",
+    choices: [
+      "toLocaleUpperCase()",
+      "toUpperCase()",
+      "toString()",
+      "valueOf()",
+    ],
+    answer: "valueOf()",
+  },
+  {
+    title:
+      "Which of the following functions of String objects causes a string to be displayed in the specified color as if it were in a <font color='color'> tag?",
+    choices: ["fixed()", "fontcolor()", "blink()", "bold()"],
+    answer: "fontcolor()",
+  },
+  {
+    title:
+      "Which of the following functions of Array objects joins all elements of an array into a string?",
+    choices: ["concat()", "join()", "pop()", "map()"],
+    answer: "join()",
+  },
+  {
+    title:
+      "Which of the following functions of Array objects returns true if at least one element in this array satisfies the provided testing function?",
+    choices: ["reverse()", "shift()", "slice()", "some()"],
+    answer: "some()",
+  },
+];
